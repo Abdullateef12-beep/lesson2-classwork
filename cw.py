87c1179d5dbb192aaf2d92cb4c3ae3137d66dca7
Abdullateef12-beep <Abdullateef.Bello1212@gmail.com>
2024-12-16 17:54:34 +0000

    Add files via upload

---
+++ cw.py
@@ -0,0 +1,19 @@
+
+import pgzrun
+from random import randint
+
+WIDTH = 500
+HEIGHT = 500
+
+
+def draw():
+
+    rect = Rect((0,0) , (300,200))
+
+
+    
+    rect.center = ((150,150))
+ 
+
+    screen.draw.rect(rect , (10,255,220))    
+pgzrun.go()
